db6c54a0527468881f32d261f97efc5120c3e8a4
Hinata Chikao <StubbornStruggle@gmail.com>
2026-08-16 03:20:58 +0330

    add header specifications to the mp.rs template

--- mp.rs
+++ mp.rs
@@ -1,12 +1,26 @@
-//! My Tiny Rust Programming Language Code
-//! Date: 
-//! Goal: 
-//! Dependency: 
-//! Rust Version: nightly 1.99.0
-//! For: Analyze, review and writing idiomatic version
+// Date: Thu Aug 15 2026
+
+// Project: Review Rust version 1.97.0
+// Goal: ...
+// Dependency: Without dependency
+
+// Hardware Mode: Lenovo IdeaPad Z500
+// Processor: Intel® Core™ i7-3612QM × 8
+
+// OS: Ubuntu 26.04 LTS x86_64 GNU/Linux
+// Kernel Version: 7.0.14-070014-generic
+// Firmware Version: 71CN51WW(V1.21)
+
+// Graphics: Intel® HD Graphics 4000 (IVB GT2)
+// GNOME Version: 50
+// Windowing System: Wayland
+
+// rustc 1.99.0-nightly (d453bdd8f 2026-08-14)
+// cargo 1.99.0-nightly (eb98b54bc 2026-08-11)
+// rustup 1.29.0 (28d1352db 2026-03-05)
 
 fn main() {
     println!("\n");
 
-    println!("\nThe End ...");
+    println!("\nThe End ...\n");
 }
