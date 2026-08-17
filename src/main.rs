427d9153a85e76952becdfefb3b988ed8cdadbb7
Hinata Chikao <StubbornStruggle@gmail.com>
2026-08-17 19:15:51 +0330

    How to use struct. Test partial move

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
-// Date: Thu Aug 15 2026
+// Date: Thu Aug 17 2026
 
-// Project: Review Rust version 1.97.0
-// Goal: ...
+// Project: Review The Book Rust Programming Language
+// Goal: Using struct
 // Dependency: Without dependency
 
-// Hardware Mode: Lenovo IdeaPad Z500
-// Processor: Intel® Core™ i7-3612QM × 8
+// rustc 1.100.0-nightly (34baba539 2026-08-16)
+// binary: rustc
+// commit-hash: 34baba5394fcbda4cba7b7c1964a6db421c77c91
+// commit-date: 2026-08-16
+// host: x86_64-unknown-linux-gnu
+// release: 1.100.0-nightly
+// LLVM version: 23.1.0
 
-// OS: Ubuntu 26.04 LTS x86_64 GNU/Linux
+// cargo 1.100.0-nightly (8a0d8afba 2026-08-15)
+// release: 1.100.0-nightly
+// commit-hash: 8a0d8afba810304bcf9a10bac430be80dd470233
+// commit-date: 2026-08-15
+// host: x86_64-unknown-linux-gnu
+// libgit2: 1.9.6 (sys:0.21.0 vendored)
+// libcurl: 8.21.0-DEV (sys:0.4.90+curl-8.21.0 vendored ssl:OpenSSL/3.6.3)
+// ssl: OpenSSL 3.6.3 9 Jun 2026
+// os: Ubuntu 26.4.0 (resolute) [64-bit]
 // Kernel Version: 7.0.14-070014-generic
 // Firmware Version: 71CN51WW(V1.21)
 
-// Graphics: Intel® HD Graphics 4000 (IVB GT2)
-// GNOME Version: 50
-// Windowing System: Wayland
-
-// rustc 1.99.0-nightly (d453bdd8f 2026-08-14)
-// cargo 1.99.0-nightly (eb98b54bc 2026-08-11)
 // rustup 1.29.0 (28d1352db 2026-03-05)
 
 fn main() {
