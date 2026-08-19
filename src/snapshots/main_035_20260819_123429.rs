92d95b65c27c020bff26c664c5b2e5d8bbcc8acb
Hinata Chikao <StubbornStruggle@gmail.com>
2026-08-19 12:37:49 +0330

    show how to iteration on vector

---
+++ src/snapshots/main_035_20260819_123429.rs
@@ -0,0 +1,59 @@
+// Date: Thu Aug 19 2026
+
+// Project: Review The Book Rust Programming Language
+// Goal: how to iteration on vector
+// Dependency: Without dependency
+
+// rustc 1.100.0-nightly (34baba539 2026-08-16)
+// binary: rustc
+// commit-hash: 34baba5394fcbda4cba7b7c1964a6db421c77c91
+// commit-date: 2026-08-16
+// host: x86_64-unknown-linux-gnu
+// release: 1.100.0-nightly
+// LLVM version: 23.1.0
+
+// cargo 1.100.0-nightly (8a0d8afba 2026-08-15)
+// release: 1.100.0-nightly
+// commit-hash: 8a0d8afba810304bcf9a10bac430be80dd470233
+// commit-date: 2026-08-15
+// host: x86_64-unknown-linux-gnu
+// libgit2: 1.9.6 (sys:0.21.0 vendored)
+// libcurl: 8.21.0-DEV (sys:0.4.90+curl-8.21.0 vendored ssl:OpenSSL/3.6.3)
+// ssl: OpenSSL 3.6.3 9 Jun 2026
+// os: Ubuntu 26.4.0 (resolute) [64-bit]
+// Kernel Version: 7.0.14-070014-generic
+// Firmware Version: 71CN51WW(V1.21)
+
+// rustup 1.29.0 (28d1352db 2026-03-05)
+
+fn main() {
+    println!("\n");
+
+    let mut my_vec: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    for item in &my_vec {
+        println!("{} ", item);
+    }
+    println!("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    for item in &mut my_vec {
+        *item += 10;
+        println!("{}", item);
+    }
+    println!("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    // -----------------------------------
+
+    for item in my_vec.iter() {
+        println!("{} ", item);
+    }
+    println!("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    for item in my_vec.iter_mut() {
+        *item += 100;
+        println!("{}", item);
+    }
+    println!("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    println!("\nThe End ...\n");
+}
